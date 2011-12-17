575103535a2361f9528d6df8d152a77800cce0f4
Mark Doyle <markjohndoyle@googlemail.com>
2011-12-17 12:17:58 +0100

    Moved the UdpReceiver class.

--- src/main/java/org/hbird/mandolaypigeon/protocols/UdpReceiver.java
+++ src/main/java/org/hbird/mandolaypigeon/protocols/UdpReceiver.java
@@ -1,4 +1,4 @@
-package org.hbird.mandolaypidgeon;
+package org.hbird.mandolaypigeon.protocols;
 
 import java.net.InetSocketAddress;
 
@@ -7,15 +7,15 @@ import org.apache.mina.common.IoSession;
 import org.hbird.transport.commons.data.GenericPayload;
 
 public class UdpReceiver {
-	
+
 	/**
 	 * Processes a Camel Mina UDP datagram and turns it into a Camel generic payload object using
 	 * the source IP address as the payload layout identifier.
 	 * @param datagram UDP payload
-	 * @param ioSession 
+	 * @param ioSession
 	 * @return {@link GenericPayload}
 	 */
-	public static GenericPayload fromIpBasedPayloadIdentifier(byte[] datagram, @Header("CamelMinaIoSession") IoSession ioSession) {
+	public static GenericPayload fromIpBasedPayloadIdentifier(final byte[] datagram, @Header("CamelMinaIoSession") final IoSession ioSession) {
 		InetSocketAddress sockadd = (InetSocketAddress)ioSession.getLocalAddress();
 		String ipAddress = sockadd.getAddress().getHostAddress();
 
